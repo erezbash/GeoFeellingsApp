4f496446994d86463d4c87256fc3503165ac38dc
yairg <yairg@wix.com>
2018-01-05 12:50:26 +0200

    add example of send state

--- src/screens/types/RegisterScreen.js
+++ src/screens/types/RegisterScreen.js
@@ -1,27 +1,48 @@
 import React from "react";
 import {View, TextInput, Text, Button} from 'react-native-ui-lib';
+import {Alert} from "react-native";
 
 export default class RegisterScreen extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
     render() {
         return (
-                <View flex paddingH-25 paddingT-120>
-                    <Text blue50 text20>Register</Text>
-                    <TextInput text50 placeholder="username" dark10/>
-                    <TextInput text50 placeholder="password" secureTextEntry dark10/>
-                    <TextInput text50 placeholder="password" secureTextEntry dark10/>
-                    <View marginT-100 center>
-                        <Button
-                            onPress={() => this.props.navigator.pop({
+            <View flex paddingH-25 paddingT-120>
+                <Text blue50 text20>Register</Text>
+                <TextInput
+                    text50 dark10
+                    placeholder="username"
+                    onChangeText={(text) => this.setState({username: text})}
+                />
+                <TextInput
+                    text50 dark10 secureTextEntry
+                    placeholder="password"
+                    onChangeText={(text) => this.setState({password: text})}
+                />
+                <TextInput
+                    text50 dark10 secureTextEntry
+                    placeholder="passwordAgain"
+                    onChangeText={(text) => this.setState({passwordAgain: text})}
+                />
+                <View marginT-100 center>
+                    <Button
+                        onPress={() => {
+                            Alert.alert(JSON.stringify(this.state, null, 2));
+                            this.props.navigator.pop({
                                 animated: true,
                                 animationType: 'fade'
-                            })}
-                            text70
-                            white
-                            background-orange30
-                            label="Register"/>
-                    </View>
+                            })
+                        }}
+                        text70
+                        white
+                        background-orange30
+                        label="Register"/>
                 </View>
+            </View>
         );
     }
 }
