556b6fd19c3a0d46a03b5a1a2974afa98b79cfb4
Erez Bashari <erez1004@gmail.com>
2018-01-05 18:38:01 +0200

    hold login state, set token and style

--- src/screens/types/LoginScreen.js
+++ src/screens/types/LoginScreen.js
@@ -1,18 +1,44 @@
 import React from "react";
+import {Alert, AsyncStorage} from 'react-native';
 import {View, TextInput, Text, Button} from 'react-native-ui-lib';
-import {startApp} from "../../app";
+import {startApp, setToken} from "../../app";
 
 export default class LoginScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
 
     render() {
         return (
             <View flex paddingH-25 paddingT-120>
                 <Text blue50 text20>Welcome</Text>
-                <TextInput text50 placeholder="username" dark10/>
-                <TextInput text50 placeholder="password" secureTextEntry dark10/>
+                <View marginT-30 />
+                <TextInput
+                    text50 dark10
+                    placeholder="username"
+                    onChangeText={(text) => this.setState({username: text})}
+                />
+                <TextInput
+                    text50 dark10 secureTextEntry
+                    placeholder="password"
+                    onChangeText={(text) => this.setState({password: text})}
+                />
                 <View marginT-100 center>
                     <Button
-                        onPress={() => startApp('after-login')}
+                        onPress={async () => {
+                            try {
+                                //token = GET request login(this.username, this.password)
+                                var _token = 'adasd-123213dad-adasd3213-adad';
+                                setToken(_token)
+                                //await AsyncStorage.setItem('userID', 'erezbash');
+                                await AsyncStorage.setItem('userID', _token);
+                                startApp('after-login');
+                            } catch (error) {
+                                // Error saving data
+                            }
+                        }}
                         text70
                         white
                         background-orange30
