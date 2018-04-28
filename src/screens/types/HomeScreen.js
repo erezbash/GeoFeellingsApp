69cef24ca4467a39dd2311d1925dd58ea4c242f7
yairg <yairg@wix.com>
2018-04-28 16:40:37 +0300

    fix twitter on ios

--- src/screens/types/HomeScreen.js
+++ src/screens/types/HomeScreen.js
@@ -3,13 +3,26 @@ import React from 'react';
 import {ScrollView, Platform} from "react-native";
 import {startApp, clearToken} from "../../app";
 import TwitterLogin from "../../components/TwitterLogin";
-import {View, TextInput, Text, Button, Picker, Image} from 'react-native-ui-lib';
+import {View, TextInput, Text, Button, Picker, Image, Avatar, Colors} from 'react-native-ui-lib';
 import {handleMaximalQuestionnairesUpdate, handleGetUserInfo} from "../../notifcations/androidHandler";
 import tagIcon from '../../../img/edit.png';
 import LocationExample from '../../NativeMethod';
 import FacebookLogin from "../../components/FacebookLogin";
+import {StarRating} from "react-native-star-rating";
 
 export default class HomeScreen extends React.Component {
+    static navigatorButtons = {
+        rightButtons: [
+            {
+                title: 'Logout',
+                id: 'Logout',
+                buttonFontSize: 14,
+                buttonColor: 'red',
+                buttonFontWeight: '600'
+            }
+        ]
+    };
+
     constructor(props) {
         super(props);
 
@@ -23,22 +36,31 @@ export default class HomeScreen extends React.Component {
             gender: "",
             image: ""
         };
+        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+    }
+
+    onNavigatorEvent(event) {
+        if (event.type === 'NavBarButtonPress' && event.id === 'Logout')
+            this.pressLogout()
     }
 
     componentDidMount() {
         this.getAndUpdateUserProfile();
     }
 
-    getAndUpdateUserProfile(){
+    getAndUpdateUserProfile() {
         handleGetUserInfo(
-            (userProfile) =>{
+            (userProfile) => {
                 this.setState({
-                    image: userProfile.image === null? "": userProfile.image,
+                    image: userProfile.image === null ? "" : userProfile.image,
                     age: userProfile.age,
                     name: userProfile.userName,
                     gender: userProfile.gender,
                     maximalQuestionnaires: userProfile.limitQuestionnaire === null ?
-                        {label: "Unlimited", value: null} : {label: userProfile.limitQuestionnaire, value: userProfile.limitQuestionnaire}
+                        {label: "Unlimited", value: null} : {
+                            label: userProfile.limitQuestionnaire,
+                            value: userProfile.limitQuestionnaire
+                        }
                 })
             }
         );
@@ -49,76 +71,73 @@ export default class HomeScreen extends React.Component {
         handleMaximalQuestionnairesUpdate(item.value);
     }
 
+    pressLogout() {
+        clearToken();
+        if (Platform.OS === 'android') {
+            LocationExample.stopLocation();
+        }
+        startApp('login')
+    }
+
     render() {
-        const imageSource = this.state.image === "" ? require('../../../img/defaultAvatar.png'): {uri: this.state.image};
+        const imageSource = this.state.image === "" ? require('../../../img/defaultAvatar.png') : {uri: this.state.image};
         return (
             <ScrollView>
-            <View flex paddingT-5 top>
-                <View center>
-                    <Text blue50 text20>Profile</Text>
-                    <Text text30>{this.state.name}</Text>
-                    <Image
-                        style={{borderRadius:50, width: 100, height: 100}}
-                        source={imageSource} />
-                    <Button
-                        onPress={() => {
-                            clearToken();
-                            if (Platform.OS === 'android') {
-                                LocationExample.stopLocation();
-                            }
-                            startApp('login')
-                        }}
-                        text90
-                        labelStyle={{fontWeight: '500'}}
-                        style={{marginBottom: 20, width: 100}}
-                        size="small"
-                        background-red30
-                        enableShadow
-                        white
-                        label="Logout"
-                    />
-                </View>
-                <View padding-10>
-                    <Text text50>Details:</Text>
-                    <Text>Gender: {this.state.gender}</Text>
-                    <Text>Age: {this.state.age}</Text>
-                    <Picker
-                        value={this.state.maximalQuestionnaires}
-                        enableModalBlur={false}
-                        onChange={item => this.updateMaximalQuestionnaires(item)}
-                        topBarProps={{title: 'Select Value'}}
-                        renderPicker={({label}) => {
-                            return (
-                                <View row>
-                                    <Text>Max Questionnaires:</Text>
-                                    <Image
-                                        style={{height: 16, width: 18, resizeMode: 'contain'}}
-                                        source={tagIcon}
-                                    />
-                                    <Text>{label}</Text>
-                                </View>
-                            );
-                        }}
-                    >
-                        <Picker.Item
-                            key = "Unlimited"
-                            value={{label: "Unlimited", value: null}}
-                        />
-                        {_.range(2, 24).map(option =>
+                <View flex top>
+                    <View center>
+                        <View padding-10 center bg-red50 style={{alignSelf: 'stretch'}}>
+                            <Avatar
+                                size={150}
+                                imageSource={imageSource}/>
+                            <Text white text30>{this.state.name}</Text>
+                            <StarRating
+                                disabled={false}
+                                maxStars={5}
+                                rating={2}
+                            />
+                        </View>
+                    </View>
+                    <View padding-10>
+                        <Text text50>Details:</Text>
+                        <Text>Gender: {this.state.gender}</Text>
+                        <Text>Age: {this.state.age}</Text>
+                        <Picker
+                            value={this.state.maximalQuestionnaires}
+                            enableModalBlur={false}
+                            onChange={item => this.updateMaximalQuestionnaires(item)}
+                            topBarProps={{title: 'Select Value'}}
+                            renderPicker={({label}) => {
+                                return (
+                                    <View row>
+                                        <Text>Max Questionnaires:</Text>
+                                        <Image
+                                            style={{height: 16, width: 18, resizeMode: 'contain'}}
+                                            source={tagIcon}
+                                        />
+                                        <Text>{label}</Text>
+                                    </View>
+                                );
+                            }}
+                        >
                             <Picker.Item
-                               key = {option}
-                               value = {{label: option, value: option}}
-                            />,
-                        )}
+                                key="Unlimited"
+                                value={{label: "Unlimited", value: null}}
+                            />
+                            {_.range(2, 24).map(option =>
+                                <Picker.Item
+                                    key={option}
+                                    value={{label: option, value: option}}
+                                />,
+                            )}
 
-                    </Picker>
+                        </Picker>
+                    </View>
+                    <View padding-10>
+                        <Text text50>Social Networks:</Text>
+                        <TwitterLogin/>
+                        <FacebookLogin/>
+                    </View>
                 </View>
-                <View padding-10>
-                    <Text text50>Social Networks:</Text>
-                    <TwitterLogin/>
-                    <FacebookLogin/>
-                </View>
-            </View>
             </ScrollView>
         );
     }
